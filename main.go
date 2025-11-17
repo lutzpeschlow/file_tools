360a1fa9521f67ebae1b7865f2d9bf9e2925e324
lutzpeschlow <lutz.peschlow@gmail.com>
2025-11-17 17:45:41 +0100

    removed gioui after testing under mint linux

--- main.go
+++ main.go
@@ -2,15 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
+
 	"os"
 	"runtime"
-
-	"gioui.org/app"
-	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/widget"
-	"gioui.org/widget/material"
 
 	// separator - standard vs. own
 	"github.com/lutzpeschlow/file_tools/ctrl"
@@ -41,75 +35,8 @@ func main() {
 	// short test of file list
 	err_siz := sizing.GetFileList(&ctrl_obj, &file_obj)
 
-	if ctrl_obj.View == "text" {
-		fmt.Print("text output")
-	}
-
-	if ctrl_obj.View == "window" {
-		go func() {
-			var w app.Window
-			w.Option(app.Title("Gio Beispiel"))
-			if err := loop(&w); err != nil {
-				log.Fatal(err)
-			}
-			os.Exit(0)
-		}()
-		app.Main()
-	}
-
 	// error value
 	fmt.Print("finalize: ", err_siz)
 }
 
 // =====================================================
-
-// =====================================================
-
-func loop(w *app.Window) error {
-	th := material.NewTheme()
-	// th.Shaper = app.NewShaper(gofont.Collection())
-
-	var ops op.Ops
-	var list layout.List
-	list.Axis = layout.Vertical // wird standardmäßig gesetzt, explizit gut
-
-	items := []string{"Item 1",
-		"Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9",
-		"Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9",
-		"Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9",
-		"Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9",
-		"Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9",
-		"Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9",
-		"Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9",
-		"Item 10"}
-	btns := make([]widget.Clickable, len(items))
-
-	for {
-		e := w.Event()
-		switch e := e.(type) {
-		case app.DestroyEvent:
-			return e.Err
-		case app.FrameEvent:
-			gtx := app.NewContext(&ops, e)
-
-			// Container mit fester Höhe und Breite, damit Scrollbar sichtbar wird
-			layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-					return layout.Dimensions{Size: gtx.Constraints.Min}
-				}),
-				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-					// Listbox mit Scrollbar
-					return list.Layout(gtx, len(items), func(gtx layout.Context, i int) layout.Dimensions {
-						btn := &btns[i]
-						if btn.Clicked(gtx) {
-							log.Println("Clicked:", items[i])
-						}
-						return material.Button(th, btn, items[i]).Layout(gtx)
-					})
-				}),
-			)
-
-			e.Frame(gtx.Ops)
-		}
-	}
-}
